db932289ee7c0674c7c6c9385f2d697208ac585a
cmateu <cmateu@astrosen.unam.mx>
2015-02-19 16:40:35 -0800

    output ext made optional for mgc3_get. default extension makes more sense now

--- bin/mgc3_get_pole_stars.py
+++ bin/mgc3_get_pole_stars.py
@@ -17,7 +17,6 @@ parser = argparse.ArgumentParser(description=__what__)
 parser.add_argument('parfile',metavar='parameter_file',help='Input catalogue parameter file',action='store',nargs=1)
 parser.add_argument('infile',metavar='data_file',help='Input catalogue file',nargs=1,action='store')
 parser.add_argument('polelist',metavar='pole_list',help='List of pole coordinates [ID phi_pole theta_pole]',nargs=1,action='store')
-#parser.add_argument('ext_prefix',metavar='outfile_ext',help='Output file prefix (output will be infile.outfile_ext.mgc3.cts)',nargs=1,action='store')
 parser.add_argument('-ext',metavar='outfile_ext',help='Output suffix [optional]. If given output will be infile.outfile_ext.mgc3.pst',action='store',default=['',],nargs=1)
 parser.add_argument('-ppar','--print_parf',help='Print sample parameter file mgc3.par and exit', action='store_true')
 parser.add_argument('-m',help='Select stars using mGC3/nGC3/GC3 method criteria. Default is mGC3', action='store',default='mGC3',choices=['mGC3','nGC3','GC3'])
@@ -49,18 +48,25 @@ print 'Reading pole list file %s ... ' % (polelistname)
 polelist=scipy.genfromtxt(polelistname,comments='#',usecols=(0,1,2))
 if np.ndim(polelist)==1: polelist=np.array([polelist,])
 
-#Cicle over pole list, one by one
-ext='%s.%s.pst' % (ext_prefix,args.m.lower())
-outfilename=filename.replace('.dat','')+ext
+#Figure the output filename
+eind=polelistname.rfind('.'+args.m.lower()) #Find .mgc3, .ng3, .gc3 extension (the dot is needed to avoid confusion for gc3)
+if eind==-1: 
+  sys.exit('WARNING: Method extension %s not found in pls filename.\nCheck the consistency of selected method to extract stars with the one used for the pole list provided.\nExiting...' % (args.m.lower()))
+if 'peak.pls' in polelistname: pext='peak.pst' 
+else: pext='pst'
+outfilename='%s%s.%s.%s' % (polelistname[:eind],ext_prefix,args.m.lower(),pext)
 print 'Printing output file %s ...' % (outfilename)
+
 #Open output file and print the exact same header the input file had, with an extra column
 outfile=open(outfilename,'w')
 outfile.write('#Stars selected according to %s criteria\n' % (args.m))
 scipy.savetxt(outfile,head,fmt='%s')
+
 #The masks associated to each pole will be combined with OR, this way, each star can only be printed once,
 #even if associated to more than one pole
 indep_mask=np.zeros(obsdata[:,0].size,dtype=bool)
 indep_pole_ids=-1*np.ones(obsdata[:,0].size)
+#Cicle over pole list, one by one
 for id_pole,phi_pole,theta_pole in polelist:
   mygrid=mgc3_lib.pole_grid(poles=[phi_pole,theta_pole])
   cat_mask=mygrid.mgc3_allobs_one_pole(obsdata,pars=survey_pars,return_mask=args.m)
@@ -75,3 +81,7 @@ for id_pole,phi_pole,theta_pole in polelist:
 print_data=obsdata[indep_mask,:].T
 indep_pole_ids=indep_pole_ids[indep_mask]
 scipy.savetxt(outfile,np.vstack([print_data,indep_pole_ids]).T)
+
+print 'Output file written: %s' % (outfilename)
+
+
