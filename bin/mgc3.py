20cbce606e60b5fccc3a4cdf773f6916a217aa34
cmateu <cmateu@astrosen.unam.mx>
2016-02-15 12:58:33 -0500

    -l option added to mgc3.py

--- bin/mgc3.py
+++ bin/mgc3.py
@@ -65,61 +65,72 @@ parser = argparse.ArgumentParser(description=__what__)
 parser.add_argument('parfile',metavar='parameter_file',help='Input catalogue parameter file',action='store',nargs=1)
 parser.add_argument('infile',metavar='data_file',help='Input catalogue file',nargs=1,action='store')
 parser.add_argument('ext_prefix',metavar='outfile_ext',help='Output file prefix (output will be infile.outfile_ext.mgc3.cts)',nargs=1,action='store')
+parser.add_argument("-l", "--llist", action="store_true",help='Take infile as list of mgc3.cts files')
 parser.add_argument('-farea',help='For non-allsky surveys, compute area fraction correction', action='store_true')
-#parser.add_argument('-ppar','--print_parf',help='Print sample parameter file mgc3.par and exit', action='store_true')
 parser.add_argument('-ppar','--print_parf',help='Print sample parameter file mgc3.par and exit',action=mgc3_lib.print_parfile_action,nargs=0)
 parser.add_argument('-v',"--version", help="Print program version", action="version",version='Version '+__version__)
 parser.add_argument('-doc',"--documentation", help="Print short program description", action="version",version=__what__)
 
 args = parser.parse_args()
-parfile, filename, ext_prefix = args.parfile[0], args.infile[0], args.ext_prefix[0]
-
-#If asked for by user, print sample parameter file
-#if args.print_parf: 
-#  mgc3_lib.print_sample_parfile()
-#  sys.exit('Exiting.')
+parfile, ext_prefix = args.parfile[0], args.ext_prefix[0]
 
 #Parse parameter file
 print 'Reading Parameter file %s ...' % (parfile)
 survey_pars=mgc3_lib.parse_pars(parfile)
 
-print 'Reading input file %s ...' % (filename)
-obsdata,filename=mgc3_lib.read_inputcat_for_mgc3(filename,pardic=survey_pars)
-print 'Input file shape (rows,cols): ', obsdata.shape
+#Parse inputs
 
-print 'Initializing pole grid...'
-mygrid=mgc3_lib.pole_grid(poles=survey_pars['grid_step'],pole_grid_dic=survey_pars)
-
-print 'Computing pole counts...'
-mygrid.mgc3(obsdata,pars=survey_pars)
-
-if args.farea:
-  print 'Initializing auxiliary pole grid for farea computation...'
-  mygrid_foot=mgc3_lib.pole_grid(poles=survey_pars['grid_step'])
-  foot_survey,foot_survey_pars=mygrid_foot.get_uniform_survey_footprint(obsdata,pars=survey_pars)
-  mygrid_foot.mgc3(foot_survey,pars=foot_survey_pars)
+if not args.llist:
+ print 'Reading file: %s' % (args.infile)
+ file_list=[args.infile[0],]
 else:
-  mygrid_foot=mygrid
-  mygrid_foot.farea=mygrid_foot.farea*0.+1.  
+ print 'Reading input files from list file: ', args.infile[0]
+ file_list=scipy.genfromtxt(args.infile[0],dtype='S')
+ if ndim(file_list)==0: file_list=array([file_list,])
 
-outfilename=filename.replace('.dat','')+'.'+ext_prefix+'.mgc3.cts'
-print 'Printing output file %s ...' % (outfilename)
-ofile=open(outfilename,'w')
-ofile.write('#---------------------------Input Parameters--------------------------------\n')
-ofile.write('#Parameter file: %s\n' % (parfile))
-ofile.write('#lon_col=%-2d,    lat_col=%-2d,    par_col=%-2d\n' % (survey_pars['lon_col']+1,survey_pars['lat_col']+1,survey_pars['par_col']+1))
-ofile.write('#pm_lon_col=%-2d, pm_lat_col=%-2d, vrad_col=%-2d\n' % (survey_pars['pm_lon_col']+1,survey_pars['pm_lat_col']+1,survey_pars['vrad_col']+1))
-ofile.write('#deg=%s, coo_glactc=%s, par_muas=%s\n' % (survey_pars['deg'],survey_pars['coo_glactc'],survey_pars['par_muas']))
-ofile.write('#pm_lon_red=%s, pm_muas=%s\n' % (survey_pars['pm_lon_red'],survey_pars['pm_muas']))
-ofile.write('#tol_r=%s, tol_v=%s, grid_step=%s\n' % (survey_pars['tol_r'],survey_pars['tol_v'],survey_pars['grid_step']))
-#If auxcols used, print them as well
-naux_pars=int((len(survey_pars.keys())-19)/3.)
-for na in range(naux_pars):
- auxl='AUX%d' % (na+1)
- ak1,ak2,ak3=auxl+'_col',auxl+'_o',auxl+'_f'
- ofile.write('#%s=%s, %s=%s, %s=%s\n' % (ak1,survey_pars[ak1]+1,ak2,survey_pars[ak2],ak3,survey_pars[ak3]))
-ofile.write('#---------------------------------------------------------------------------\n')
-ofile.write("#%9s %10s %10s %10s %10s %10s %10s\n" % ("phi","theta","np_mgc3gal","np_mgc3hel",'np_gc3gal',"np_ngc3gal",'farea'))
-scipy.savetxt(ofile,array([mygrid.l,mygrid.b,mygrid.np_mgc3,mygrid.mgc3hel,mygrid.np_gc3,mygrid.np_ngc3,mygrid_foot.farea]).T,fmt='%10.3f %10.3f %10d %10d %10d %10d %10.4f')
+jj=0
+for filename in file_list:
+
+  jj=jj+1
+  print 'Reading input file %s (%d of %d) ...' % (filename,jj,len(file_list))
+
+  obsdata,filename=mgc3_lib.read_inputcat_for_mgc3(filename,pardic=survey_pars)
+  print 'Input file shape (rows,cols): ', obsdata.shape
+  
+  print 'Initializing pole grid...'
+  mygrid=mgc3_lib.pole_grid(poles=survey_pars['grid_step'],pole_grid_dic=survey_pars)
+  
+  print 'Computing pole counts...'
+  mygrid.mgc3(obsdata,pars=survey_pars)
+  
+  if args.farea:
+    print 'Initializing auxiliary pole grid for farea computation...'
+    mygrid_foot=mgc3_lib.pole_grid(poles=survey_pars['grid_step'])
+    foot_survey,foot_survey_pars=mygrid_foot.get_uniform_survey_footprint(obsdata,pars=survey_pars)
+    mygrid_foot.mgc3(foot_survey,pars=foot_survey_pars)
+  else:
+    mygrid_foot=mygrid
+    mygrid_foot.farea=mygrid_foot.farea*0.+1.  
+  
+  outfilename=filename.replace('.dat','')+'.'+ext_prefix+'.mgc3.cts'
+  print 'Printing output file %s ...' % (outfilename)
+  ofile=open(outfilename,'w')
+  ofile.write('#---------------------------Input Parameters--------------------------------\n')
+  ofile.write('#Parameter file: %s\n' % (parfile))
+  ofile.write('#lon_col=%-2d,    lat_col=%-2d,    par_col=%-2d\n' % (survey_pars['lon_col']+1,survey_pars['lat_col']+1,survey_pars['par_col']+1))
+  ofile.write('#pm_lon_col=%-2d, pm_lat_col=%-2d, vrad_col=%-2d\n' % (survey_pars['pm_lon_col']+1,survey_pars['pm_lat_col']+1,survey_pars['vrad_col']+1))
+  ofile.write('#deg=%s, coo_glactc=%s, par_muas=%s\n' % (survey_pars['deg'],survey_pars['coo_glactc'],survey_pars['par_muas']))
+  ofile.write('#pm_lon_red=%s, pm_muas=%s\n' % (survey_pars['pm_lon_red'],survey_pars['pm_muas']))
+  ofile.write('#tol_r=%s, tol_v=%s, grid_step=%s\n' % (survey_pars['tol_r'],survey_pars['tol_v'],survey_pars['grid_step']))
+  #If auxcols used, print them as well
+  naux_pars=int((len(survey_pars.keys())-19)/3.)
+  for na in range(naux_pars):
+   auxl='AUX%d' % (na+1)
+   ak1,ak2,ak3=auxl+'_col',auxl+'_o',auxl+'_f'
+   ofile.write('#%s=%s, %s=%s, %s=%s\n' % (ak1,survey_pars[ak1]+1,ak2,survey_pars[ak2],ak3,survey_pars[ak3]))
+  ofile.write('#---------------------------------------------------------------------------\n')
+  ofile.write("#%9s %10s %10s %10s %10s %10s %10s\n" % ("phi","theta","np_mgc3gal","np_mgc3hel",'np_gc3gal',"np_ngc3gal",'farea'))
+  scipy.savetxt(ofile,array([mygrid.l,mygrid.b,mygrid.np_mgc3,mygrid.mgc3hel,mygrid.np_gc3,mygrid.np_ngc3,mygrid_foot.farea]).T,fmt='%10.3f %10.3f %10d %10d %10d %10d %10.4f')
+  ofile.close()
 print 'Done'
 
